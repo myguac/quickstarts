7086f0c18eb3a4881a3c8b44d39a737b7faa9182
Jeff McAffer <jeffmcaffer@github.com>
2022-05-20 15:05:38 -0700

    Update created.js

--- .github/apps/hubble/events/issue_comment/created.js
+++ .github/apps/hubble/events/issue_comment/created.js
@@ -1,4 +1,10 @@
-export default function ({ event }) {
+import { kv } from '@extendohub/storage'
+
+export default async ({event}) => {
   const newWords = event.comment.body.match(/\b\w{5}\b/g)
-  console.log(`Five letter words:\n${newWords.join('\n')}`)
+  if (!newWords || !newWords.length) return
+  const currentWords = await kv.get('words')
+  const words = new Set(currentWords)
+  newWords.forEach(word => words.add(word.toLowerCase()))
+  await kv.set('words', Array.from(words))
 }
