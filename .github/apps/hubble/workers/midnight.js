6381e9f6fa43da3d4eb8395a2186727e08786257
Jeff McAffer <jeffmcaffer@github.com>
2022-05-20 15:07:31 -0700

    Create midnight.js

---
+++ .github/apps/hubble/workers/midnight.js
@@ -0,0 +1,10 @@
+import { kv } from '@extendohub/storage'
+
+export default async function () {
+  const words = await kv.get('words')
+  if (!words) throw new Error('No word list for hubble')
+  const index = Math.floor(Math.random() * words.length)
+  const word = words[index]
+  await kv.set('word', word)
+  console.log(`Picked new word of the day: ${word}`)
+}
